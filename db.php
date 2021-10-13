9ccac44dab1c4717e3c565cea28a76c4eea9fd45
norangmoran <87406167+norangmoran@users.noreply.github.com>
2021-10-14 01:47:34 +0900

    알람 한글깨짐...
    db.php의 header에 인코더가 UTF-8이 아니라 UTP-8이었다.
    오타...

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-    header('Content-Type: text/html; charset=UTP-8');
+    header('Content-Type: text/html; charset=UTF-8');
 
     // localhost = DB주소, web=DB계정아이디, 1234=DB계정비밀번호, post_board="DB이름"
     $db = new mysqli("localhost","ran","1234","eventboard","3306");
