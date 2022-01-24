4cbd54e9bdb484919c632d79837d61a9a79e287f
norangmoran <87406167+norangmoran@users.noreply.github.com>
2022-01-24 23:24:41 +0900

    댓글 수정 pw확인
    pw확인하는 기능 넣음

--- page/board/reply_modify.php
+++ page/board/reply_modify.php
@@ -9,6 +9,11 @@ $sql2 = SQLsyn("SELECT * from board where no='".$num."'");
 $board = $sql2->fetch_array();
 
 $sql3 = SQLsyn("UPDATE reply set content='".$_POST['content']."' where no='".$rno."'");
-?>
-<script type="text/javascript">alert('수정되었습니다.');
-location.replace("read.php?no=<?php echo $num; ?>"); </script>
+
+if(password_verify($_POST['pw'],$reply['pw'])) { ?>
+  <script type="text/javascript">alert('수정되었습니다.');
+  location.replace("read.php?no=<?php echo $num; ?>");</script>
+<?php } else { ?>
+  <script type="text/javascript">alert('비밀번호가 틀립니다.');
+  history.back();</script>
+<?php } ?>
