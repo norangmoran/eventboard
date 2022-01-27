cc2d04a9786aa42550dbd41804aef4139cfa8076
norangmoran <87406167+norangmoran@users.noreply.github.com>
2022-01-27 23:58:34 +0900

    댓글에 시간 자동생성, 표시
    댓글에 시간을 자동으로 생성하고 읽을 때 표시되게 함. 내일은 캐싱을...!

--- replyON.php
+++ replyON.php
@@ -1,12 +1,14 @@
 <?php
   include $_SERVER['DOCUMENT_ROOT']."/eventboard/db.php";
 
+  $time = date('Y-m-d H:i:s');
   $num=$_GET['no'];
   $userpw=password_hash($_POST['dat_pw'], PASSWORD_DEFAULT);
   // $time = date('Y-m-d H:i:s');
 
   if($num && $_POST['dat_user'] && $userpw && $_POST['content']) {
-    $sql = SQLsyn("insert into reply(con_num,name,pw,content) values('".$num."','".$_POST['dat_user']."','".$userpw."','".$_POST['content']."')");
+    $sql = SQLsyn("insert into reply(con_num,name,pw,content,time) 
+      values('".$num."','".$_POST['dat_user']."','".$userpw."','".$_POST['content']."','".$time."')");
     echo "<script>alert('댓글이 등록되었습니다.');
     location.href='/eventboard/page/board/read.php?no=$num';</script>";
   } else {
